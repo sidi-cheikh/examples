1ea5c327d3316af300764938e706c78f8dd0d491
Sidi Cheikh <chei8547@stthomas.edu>
2017-09-23 01:09:33 +0000

    2nd script commit

---
+++ chei8547-Lab02.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+apt-get install -y tree
+apt-get install -y git
+
+mkdir A E H
+mkdir A/B A/C A/D
+mkdir E/F E/G
+mkdir H/I H/J H/M 
+mkdir H/J/K H/J/L
+
+touch A/aa A/B/bb A/C/cc A/D/dd E/ee E/F/ff E/G/gg H/hh H/I/ii H/J/jj 
+touch H/J/K/kk H/J/L/ll H/M/mm
+
+
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
